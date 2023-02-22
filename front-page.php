a7ce9ff3e350c1e268be54b56678b1f9d110080f
Brandon Vadillo <brandon.vadillo@devitm.com>
2023-02-22 17:01:27 -0600

    Modificacion front page para hacer slider dinamico
    El slider ahora es dinamico y se le puede agregar varias slides
    contiene un titulo, texto, imagen, 2 botones cada uno con su texto y la pagina a la que debe de llevar y tambien se puede agregar un video

--- front-page.php
+++ front-page.php
@@ -31,68 +31,63 @@ get_header();
     <!-- SLIDER AREA START (slider-3) -->
     <div class="ltn__slider-area ltn__slider-3  section-bg-1">
         <div class="ltn__slide-one-active slick-slide-arrow-1 slick-slide-dots-1">
-            <!-- ltn__slide-item -->
-            <div class="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3-normal ltn__slide-item-3">
-                <div class="ltn__slide-item-inner">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12 align-self-center">
-                                <div class="slide-item-info">
-                                    <div class="slide-item-info-inner ltn__slide-animation">
-                                        <div class="slide-video mb-50 d-none">
-                                            <a class="ltn__video-icon-2 ltn__video-icon-2-border" href="https://www.youtube.com/embed/tlThdr3O5Qo" data-rel="lightcase:myCollection">
-                                                <i class="fa fa-play"></i>
-                                            </a>
-                                        </div>
-                                        <h6 class="slide-sub-title white-color--- animated"><span><i class="fas fa-home"></i></span> Real Estate Agency</h6>
-                                        <h1 class="slide-title animated ">Find Your Dream <br> House By Us</h1>
-                                        <div class="slide-brief animated">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                                        </div>
-                                        <div class="btn-wrapper animated">
-                                            <a href="about.html" class="theme-btn-1 btn btn-effect-1">Make An Enquiry</a>
-                                            <a class="ltn__video-play-btn bg-white" href="https://www.youtube.com/embed/HnbMYzdjuBs?autoplay=1&amp;showinfo=0" data-rel="lightcase">
-                                                <i class="icon-play  ltn__secondary-color"></i>
-                                            </a>
+            <?php 
+            ?>
+            <?php 
+                $slides = get_post_meta(get_the_ID(), 'front_page_grupo_slider1',true);
+                $i_slides = 1;
+                foreach($slides as $slide){
+                    $extra_slide_class = ($i_slides%2 == 0) ? 'text-right' : '';
+                    $extra_img_class = ($i_slides%2 == 0) ? 'slide-img-left' : '';
+                    $extra_h1_class = ($i_slides%2 == 0) ? 'pl-5' : 'col-10';
+            ?>
+                <!-- ltn__slide-item -->
+                <div class="ltn__slide-item ltn__slide-item-2 ltn__slide-item-3-normal ltn__slide-item-3">
+                    <div class="ltn__slide-item-inner <?php echo esc_attr($extra_slide_class); ?>">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12 align-self-center">
+                                    <div class="slide-item-info">
+                                        <div class="slide-item-info-inner ltn__slide-animation">
+                                            <div class="slide-video mb-50 d-none">
+                                                <a class="ltn__video-icon-2 ltn__video-icon-2-border" href="https://www.youtube.com/embed/tlThdr3O5Qo" data-rel="lightcase:myCollection">
+                                                    <i class="fa fa-play"></i>
+                                                </a>
+                                            </div>
+                                            <h6 class="slide-sub-title white-color--- animated"><span><i class="fas fa-home"></i></span> Real Estate Agency</h6>
+                                            <h1 class="slide-title animated <?php echo esc_attr($extra_h1_class); ?>"><?php echo $slide['title']; ?></h1>
+                                            <div class="slide-brief animated">
+                                                <p><?php echo $slide['description']; ?></p>
+                                            </div>
+                                            <div class="btn-wrapper animated">
+                                                <?php if (isset($slide['text_btn_1']) && strlen($slide['text_btn_1']) > 0){?>
+                                                <a href="<?php echo (isset($slide['selec_btn_1'])) ? esc_attr( $slide['selec_btn_1'] ) : '#' ?>" class="theme-btn-1 btn btn-effect-1"><?php echo $slide['text_btn_1'] ?></a>
+                                                <?php } ?>
+                                                <?php if(isset($slide['text_btn_2']) && strlen($slide['text_btn_2']) > 0){?>
+                                                    <a href="<?php echo (isset($slide['selec_btn_2'])) ? esc_attr( $slide['selec_btn_2'] ) : '#' ?>" class="btn btn-transparent btn-effect-3"><?php echo $slide['text_btn_2']; ?></a>
+                                                <?php } ?>
+                                                <?php if (isset($slide['video']) && strlen($slide['video']) > 0){ 
+                                                    $url_embed = preg_replace(
+                                                        "/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i",
+                                                        "www.youtube.com/embed/$2",
+                                                        $slide['video']);
+                                                    ?>
+                                                <a class="ltn__video-play-btn bg-white" href="<?php echo esc_url($url_embed).'?autoplay=1&showinfo=0'; ?>" data-rel="lightcase">
+                                                    <i class="icon-play  ltn__secondary-color"></i>
+                                                </a>
+                                                <?php }?>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="slide-item-img">
-                                    <img src="<?php echo esc_url(get_template_directory_uri(  ))?>/assets/img/slider/21.png" alt="#">
+                                    <div class="slide-item-img <?php echo esc_attr($extra_img_class); ?>">
+                                        <img src="<?php echo $slide['image']; ?>" alt="#">
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <!-- ltn__slide-item -->
-            <div class="ltn__slide-item ltn__slide-item-2  ltn__slide-item-3-normal ltn__slide-item-3">
-                <div class="ltn__slide-item-inner  text-right">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12 align-self-center">
-                                <div class="slide-item-info">
-                                    <div class="slide-item-info-inner ltn__slide-animation">
-                                        <h6 class="slide-sub-title white-color--- animated"><span><i class="fas fa-home"></i></span> Real Estate Agency</h6>
-                                        <h1 class="slide-title animated ">The Right Place <br>of House Finding</h1>
-                                        <div class="slide-brief animated">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
-                                        </div>
-                                        <div class="btn-wrapper animated">
-                                            <a href="service.html" class="theme-btn-1 btn btn-effect-1">OUR SERVICES</a>
-                                            <a href="about.html" class="btn btn-transparent btn-effect-3">LEARN MORE</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="slide-item-img slide-img-left">
-                                    <img src="<?php echo esc_url(get_template_directory_uri(  ))?>/assets/img/slider/21.png" alt="#">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
+            <?php $i_slides++; } ?>
         </div>
     </div>
     <!-- SLIDER AREA END -->
