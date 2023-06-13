be18f52ec88f14fc6c43b525ce4c73f5c35d06f6
Marco Aspeitia <maspeitia.31@gmail.com>
2023-06-13 17:31:39 -0600

    new gallery in inmuebles detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -378,7 +378,28 @@
                 }
             ]
         });
-                        
+
+        $('.multiple-items').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 3
+          });
+        $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+        });
+        $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        arrows: false,
+        focusOnSelect: true
+        });
         /* --------------------------------------------------------
             13. Isotope Gallery Active  ( Gallery / Portfolio )
         -------------------------------------------------------- */
